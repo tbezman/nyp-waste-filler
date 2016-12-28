f91755b88e161a5a5550246fa4548a58fe40d8d2
Terence Bezman <terencebezman@gmail.com>
2016-12-27 19:20:28 -0500

    Implemented a back button for the work flow

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -2,21 +2,22 @@ let angular = require('angular');
 
 angular.module('app.run', [])
 	.run(($rootScope, StorageService, $state) => {
-        let stateHistory = [];
+        let states = $state.get().filter(state => {
+            return state.parent && state.parent == 'work';
+        }).map(state => state.name);
 
+        console.log(states);
 
 		$rootScope.$on('$stateChangeStart', event => {
 			StorageService.updateAll();
 		});
 
-		$rootScope.$on('$stateChangeSuccess', (event, toState) => {
-	        stateHistory.push(toState.name);
-        })
-
         $rootScope.$on('flowBack', () => {
-            if(stateHistory.length >= 2) {
-                let last = stateHistory[stateHistory.length - 2];
-                $state.go(last)
+            let index = states.indexOf($state.current.name);
+
+            if(index > 0) {
+                index--;
+                $state.go(states[index]);
             }
         });
 	});
