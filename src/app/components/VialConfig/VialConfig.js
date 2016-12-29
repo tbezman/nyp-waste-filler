59b44906f98a288d8dae2b8c5c535e7fb63f01c3
Terence Bezman <terencebezman@gmail.com>
2016-12-28 22:32:38 -0500

    Best vial config stuff

--- src/app/components/VialConfig/VialConfig.js
+++ src/app/components/VialConfig/VialConfig.js
@@ -1,9 +1,116 @@
 import fs from 'fs';
 
 class VialConfigController {
-        constructor() {
-
+    constructor() {
         this.vials = JSON.parse(fs.readFileSync(appRoot + '/vials.json', 'utf-8'));
+        this.vialCache = {};
+    }
+
+    k_combinations(set, k) {
+        var i, j, combs, head, tailcombs;
+
+        if (k > set.length || k <= 0) {
+            return [];
+        }
+
+        if (k == set.length) {
+            return [set];
+        }
+
+        if (k == 1) {
+            combs = [];
+            for (i = 0; i < set.length; i++) {
+                combs.push([set[i]]);
+            }
+            return combs;
+        }
+
+        combs = [];
+        for (i = 0; i < set.length - k + 1; i++) {
+            // head is a list that includes only our current element.
+            head = set.slice(i, i + 1);
+            // We take smaller combinations from the subsequent elements
+            tailcombs = this.k_combinations(set.slice(i + 1), k - 1);
+            // For each (k-1)-combination we join it with the current
+            // and store it to the set of k-combinations.
+            for (j = 0; j < tailcombs.length; j++) {
+                combs.push(head.concat(tailcombs[j]));
+            }
+        }
+        return combs;
+    }
+
+
+    combinations(set) {
+        var k, i, combs, k_combs;
+        combs = [];
+
+        for (k = 1; k <= set.length; k++) {
+            k_combs = this.k_combinations(set, k);
+            for (i = 0; i < k_combs.length; i++) {
+                combs.push(k_combs[i]);
+            }
+        }
+        return combs;
+    }
+
+    allPermuationsOfVialSizes(billed, config) {
+        let vialSizes = config.vial_size.split(',').map(vial => parseFloat(vial));
+        let verbose = [];
+        vialSizes.forEach(vialSize => {
+            let maxAmount = Math.ceil(parseFloat(billed) / vialSize);
+            for (var i = 0; i < maxAmount; i++)
+                verbose.push(vialSize);
+        })
+
+        let wastes = [];
+
+        let combinations = this.combinations(verbose).filter(combination => {
+            let sum = combination.reduce((a, b) => a + b, 0);
+
+            if (sum >= billed)
+                wastes.push({
+                    waste: sum - billed,
+                    config: combination
+                });
+
+            return sum >= billed;
+        });
+
+        let sorted = wastes.sort((a, b) => {
+            return a.waste - b.waste;
+        });
+
+        return sorted;
+    }
+
+    bestConfigForVial(vial, billed) {
+        let key = vial.vial_size + ':' + billed;
+
+        console.log(vial);
+        console.log(key);
+
+        if (this.vialCache.hasOwnProperty(key)) {
+            return this.vialCache[key];
+        }
+
+        let config = this.allPermuationsOfVialSizes(billed, vial)[0];
+
+        this.vialCache[key] = config;
+
+        return config;
+    }
+
+    matchingVialForDrug(drug) {
+        let drugName = drug.split(' ')[0];
+
+        this.vials.forEach(vial => {
+            if (vial.drug == drugName) {
+                return vial;
+            }
+        });
+
+        return null;
     }
 
     add() {
@@ -18,9 +125,6 @@ class VialConfigController {
         fs.writeFileSync(appRoot + '/vials.json', JSON.stringify(this.vials));
     }
 
-    $onInit() {
-        this.hello = "world";
-    }
 }
 
 import vial from './VialConfig.html';
